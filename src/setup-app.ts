5312c8e15ca186c5426d2cd7c70229b03fa88284
daniil.i <d.ivkin@aladdin.store>
2025-12-28 18:48:35 +0300

    v3

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -10,7 +10,7 @@ export const setupApp = (app: Express) => {
   app.get("/", (req, res) => {
     res.status(200).send({ message: "Hello World" });
   });
-  app.delete(ROUTES.TESTING_ALL_DATA, (req, res) => {
+  app.use(ROUTES.TESTING_ALL_DATA, (req, res) => {
     db.blogs = [];
     db.posts = [];
     res.status(204).send();
