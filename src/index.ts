4f485d7e6ce46dc572df7c73e134db0c75e7878c
daniil.i <d.ivkin@aladdin.store>
2025-12-29 09:50:46 +0300

    fix/vercel

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ setupApp(app);
 
 const PORT = process.env.PORT || 5001;
 
-// Для Railway и других платформ сервер должен запускаться всегда
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    return console.log(`Server is running on port ${PORT}`);
+  });
+}
 export default app;
